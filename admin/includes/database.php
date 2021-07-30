25a511098cffd7c533472d1002c784130095dc61
Сергей <jok@mail.ru>
2021-07-30 09:34:32 +0300

    new

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -16,15 +16,29 @@ class Database {
             die("Database connection failed badly" . mysqli_error());
         }
     }
+
+   public function query($sql) {
+        $result = mysqli_query($this->connection, $sql);
+
+        return $result;
+    }
+
+    private function confirm_query($result) {
+        if (!$result) {
+            die("Query Failed ".mysqli_error());
+        }
+    }
+
+    public function escape_string($string) {
+       $escaped_string = mysqli_real_escape_string($this->connection, $string);
+       return $escaped_string;
+    }
+
 }
 
-public function query($sql) {
-    $result = mysqli_query($this->connection, $sql); 
-    if (!$result) {
-        die("Query Failed");
-    }
-    return $result;
-}
+
+
 
 $database = new Database();
 
+?>
